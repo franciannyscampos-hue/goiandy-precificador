158df17de9aff886cd4e6439a7f9f77cfb3773af
franciannyscampos-hue <francianny.scampos@gmail.com>
2026-09-18 16:46:36 -0400

    Add files via upload

---
+++ precificador_api/precificador/__init__.py
@@ -0,0 +1,9 @@
+from .config import CatalogConfig
+from .pipeline import processar_catalogo, eh_catalogo_escaneado, juntar_pdfs, Relatorio
+from .render import gerar_apendice_faltantes, fmt_preco
+from .auto_config import detectar_config, config_padrao
+
+__all__ = [
+    'CatalogConfig', 'processar_catalogo', 'eh_catalogo_escaneado', 'juntar_pdfs',
+    'Relatorio', 'gerar_apendice_faltantes', 'fmt_preco', 'detectar_config', 'config_padrao',
+]
