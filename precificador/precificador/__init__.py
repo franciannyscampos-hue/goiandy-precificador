346b927aa2ab4b64cb371081757c80ee138b9141
franciannyscampos-hue <francianny.scampos@gmail.com>
2026-09-21 09:37:16 -0400

    Delete precificador directory

--- precificador/precificador/__init__.py
+++
@@ -1,8 +0,0 @@
-from .config import CatalogConfig
-from .pipeline import processar_catalogo, eh_catalogo_escaneado, juntar_pdfs, Relatorio
-from .render import gerar_apendice_faltantes, fmt_preco
-
-__all__ = [
-    'CatalogConfig', 'processar_catalogo', 'eh_catalogo_escaneado', 'juntar_pdfs',
-    'Relatorio', 'gerar_apendice_faltantes', 'fmt_preco',
-]
